07be65879b714dad3bcac005aed3db22a5f4e47c
Nattick <natalia.a.konovalova@gmail.com>
2023-03-11 15:06:33 +0300

    Add Ex15

--- Ex15/Program.cs
+++ Ex15/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+
+Console.Write("Enter a weekday number: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void CheckingTheDayOfTheWeek (int dayNumber) {
+  if (dayNumber == 6 || dayNumber == 7) {
+  Console.WriteLine("(Hooray weekend!)");
+  }
+  else if (dayNumber < 1 || dayNumber > 7) {
+    Console.WriteLine("It's not a weekday...");
+  }
+  else Console.WriteLine("(Keep working baby)");
+}
+
+CheckingTheDayOfTheWeek(dayNumber);
