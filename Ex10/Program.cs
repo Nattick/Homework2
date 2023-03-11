f75f060f7ea8220c9d8f859b3a1a65e364b51889
Nattick <natalia.a.konovalova@gmail.com>
2023-03-11 15:02:36 +0300

    Add Ex10

---
+++ Ex10/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+
+Console.Write("Enter a 3-digit number: ");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigitNumber);
+Console.WriteLine("Second digit is "+stringNumber[1]);
